0a2b1288d0a12fc789170dab5082e5d468072e67
Susan Holland <segh@fastmail.com>
2020-07-25 16:29:23 -0400

    Declared and called dependencies

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const db = require("./models");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+// Body parsing middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static assets
+app.use(express.static("public"));
+
+// Connect database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
