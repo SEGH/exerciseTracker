230453f58bffcfff01751390c1cf1db21ebbc342
Susan Holland <segh@fastmail.com>
2020-07-25 17:18:19 -0400

    Added html routes

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const db = require("./models");
+// const db = require("./models");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// Route logger
 app.use(logger("dev"));
 
 // Body parsing middleware
@@ -19,6 +20,15 @@ app.use(express.static("public"));
 
 // Connect database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+
+// html routes
+app.get("/stats", (req, res) => {
+    res.sendFile(__dirname + "/public/stats.html");
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(__dirname + "/public/exercise.html");
+});
 
 // Run server
 app.listen(PORT, () => {
