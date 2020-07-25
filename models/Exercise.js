c5f2aa07e912ebb38ae74751f5ff4a68807f2a20
Susan Holland <segh@fastmail.com>
2020-07-25 18:22:33 -0400

    Wrote Exercise model

---
+++ models/Exercise.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+    type: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    duration: {
+        type: Number,
+        required: true
+    },
+
+    distance: {
+        type: Number
+    },
+
+    weight: {
+        type: Number
+    },
+
+    reps: {
+        type: Number
+    },
+
+    sets: {
+        type: Number
+    }
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Exercise;
