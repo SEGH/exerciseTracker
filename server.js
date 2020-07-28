1afc3ff1c58a47f1b9b61d63e0ba95ee3da99404
Susan Holland <segh@fastmail.com>
2020-07-28 09:14:15 -0400

    Added conditions to connection

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 // html routes
 app.get("/stats", (req, res) => {
